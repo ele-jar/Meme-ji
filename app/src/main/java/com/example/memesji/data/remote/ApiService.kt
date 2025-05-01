e967e1f2a5bda49f636b18338263ad7ab4f55ad3
ele-jar <mitwenspotify@gmail.com>
2025-05-01 18:23:15 +0530

    Update ApiService.kt

--- app/src/main/java/com/example/memesji/data/remote/ApiService.kt
+++ app/src/main/java/com/example/memesji/data/remote/ApiService.kt
@@ -1,5 +1,6 @@
 package com.example.memesji.data.remote
 
+import com.example.memesji.data.AppInfo
 import com.example.memesji.data.Meme
 import retrofit2.Response
 import retrofit2.http.GET
@@ -7,4 +8,7 @@ import retrofit2.http.GET
 interface ApiService {
     @GET("ele-jar/meme-database/main/memes.json")
     suspend fun getMemes(): Response<List<Meme>>
+
+    @GET("ele-jar/meme-database/main/app_info.json")
+    suspend fun getAppUpdateInfo(): Response<AppInfo>
 }
