a399bec3b364fa1304987d4bd2351adcfc830f50
ele-jar <mitwenspotify@gmail.com>
2025-05-01 10:12:11 +0530

    Update MemesJiApp.kt

--- app/src/main/java/com/example/memesji/MemesJiApp.kt
+++ app/src/main/java/com/example/memesji/MemesJiApp.kt
@@ -1,12 +1,13 @@
 package com.example.memesji
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
-// Optional: If you need an Application class (e.g., for Hilt)
-// @HiltAndroidApp // Uncomment if using Hilt
 class MemesJiApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        // Initialization code if needed
+        
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        
     }
 }
